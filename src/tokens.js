c29a7532286cb946969356de6871278c6b78bc26
Ernesto Laval <ernesto.laval@gmail.com>
2018-04-03 17:28:28 -0300

    Cleanup unused handlers

--- src/tokens.js
+++ src/tokens.js
@@ -14,74 +14,12 @@ api.use(function(req,res,next) {
         const claims = JSON.parse(buf);
         req.claims = claims;
         next();
-    } else if (req.query.access_key) {
-        const claims = dataStorage.getClaimsFromKey(req.query.access_key)
-        .then(claims => {
-            req.claims = claims;
-            next();
-        })
     } else {
         res.status(401).error('Not Authorized')
     }
 })
 
-// Define a route
-api.get('/process', function(req,res) {
 
-    const user = req.claims && req.claims.sub;
-
-    dataStorage.getProcessCollection(user)
-    .then(d => {
-        res.status(200).json({ data:d })
-    })
-    .catch(err => {
-        res.status(200).json({ "msg":"ERROR caught in post /process", "error": err })
-    })
-})
-
-api.get('/process/:id', function(req,res) {
-    const user = req.claims && req.claims.sub;
-
-    dataStorage.getProcess(user, req.params.id)
-    .then(d => {
-        res.status(200).json({ data:d })
-    })
-})
-
-api.post('/process', (req,res) => {
-    const user = req.claims && req.claims.sub;
-
-    dataStorage.createProcess(user, req.body)
-    .then(d => {
-        res.status(200).json({ process: req.params.id, data:d })
-    })
-
-})
-
-api.post('/process/:id/event', (req,res) => {
-    const user = req.claims && req.claims.sub;
-
-    dataStorage.createEvent(user, req.params.id, req.body)
-    .then(d => {
-        res.status(200).json({ process: req.params.id, data:d })
-    })
-})
-
-api.get('/process/echo', (req,res) => {
-    const user = req.claims && req.claims.sub;
-
-    res.status(200).json({headers: req.headers, query:req.query, params : req.params, req:req})
-})
-
-
-api.post('/process/:id/event', (req,res) => {
-    const user = req.claims && req.claims.sub;
-
-    dataStorage.createEvent(user, req.params.id, req.body)
-    .then(d => {
-        res.status(200).json({ process: req.params.id, data:d })
-    })
-})
 
 api.post('/token', (req,res) => {
     const user = req.claims && req.claims.sub;
